26bb511cb7c3ad928ecf16b2b4ebaf14cf038826
Milos <miskoo995@gmail.com>
2025-07-21 10:24:57 +0200

    style changes

--- src/app/client/page.js
+++ src/app/client/page.js
@@ -1,44 +1,18 @@
 "use client";
 import { useState } from "react";
-
-import "react-big-calendar/lib/css/react-big-calendar.css";
-
-import { Calendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
+import CoreCalendar from "../components/CoreCalendar/CoreCalendar";
 import { useTranslation } from "react-i18next";
 
 export default function Home() {
   const { t } = useTranslation();
 
-  // const [startDate, setStartDate] = useState(new Date());
-  // const localizer = momentLocalizer(moment);
-  // const events = [
-  //   {
-  //     title: "My Event",
-  //     start: new Date(2025, 6, 20, 10, 0),
-  //     end: new Date(2025, 6, 20, 12, 0),
-  //   },
-  // ];
   return (
     <div className={"xl:px-12 px-6"}>
-      <div className='py-5'>
+      <div className="py-5">
         <h1>{t("welcome")}</h1>
         <p>{t("description")}</p>
+        <CoreCalendar />
       </div>
-      {/* <Calendar
-        localizer={localizer}
-        events={events}
-        startAccessor="start"
-        endAccessor="end"
-        style={{ height: 500 }}
-        selectable={true}
-        onSelectSlot={(slotInfo) => {
-          console.log("Selected slot", slotInfo);
-        }}
-        onSelectEvent={(event) => {
-          console.log("Selected event", event);
-        }}
-      /> */}
     </div>
   );
 }

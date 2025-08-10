7ece286254d078703f271d0e5440206372ee1da3
Milos <miskoo995@gmail.com>
2025-08-10 02:44:59 +0200

    changing env  variable

--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const http = axios.create({
-  baseURL: "http://localhost:3001/api/v1",
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
   headers: {
     "Content-Type": "application/json",
   },

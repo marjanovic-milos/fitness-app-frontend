72218153178d2f1e9e9aaf6cbbd2c89d4e01bf8e
Milos Marjanovic <miskoo995@gmail.com>
2025-08-14 08:31:42 +0200

    update table

--- src/components/CoreTable/CoreTable.js
+++ src/components/CoreTable/CoreTable.js
@@ -8,15 +8,7 @@ import Image from "next/image";
 import CoreInput from "../CoreInput/CoreInput";
 import { useForm } from "react-hook-form";
 const CoreTable = (props) => {
-  const {
-    columns,
-    className,
-    loading = true,
-    sortingHandler,
-    data,
-    deleteMutation,
-    updateMutation,
-  } = props;
+  const { columns, className, loading = true, sortingHandler, data, deleteMutation, updateMutation } = props;
 
   const { dark } = useContext(ThemeContext);
 
@@ -24,19 +16,15 @@ const CoreTable = (props) => {
   const { register, handleSubmit } = useForm();
 
   const root = `core-table ${className?.root}`;
-  const header = `${dark ? "core-table-header-dark" : "core-table-header"} ${
-    className?.header
-  }`;
-  const headerItem = `${
-    dark ? "core-table-header-item-dark" : "core-table-header-item"
-  } ${className?.headerItem}`;
+  const header = `${dark ? "core-table-header-dark" : "core-table-header"} ${className?.header}`;
+  const headerItem = `${dark ? "core-table-header-item-dark" : "core-table-header-item"} ${className?.headerItem}`;
 
   const loader = Array.from({ length: 5 }, (_, i) => (
-    <tr className="my-2 px-5" key={i}>
-      <td className="core-table-loader" />
-    </tr>
+    <div className='my-2 px-5' key={i}>
+      <div className='core-table-loader' />
+    </div>
   ));
-  const test = (data) => {
+  const onSubmit = (data) => {
     console.log(actionId);
     updateMutation.mutate({ data, actionId });
   };
@@ -54,62 +42,32 @@ const CoreTable = (props) => {
       return 0;
     });
     return (
-      <CoreTableRow
-        handleSubmit={handleSubmit}
-        onSubmit={test}
-        key={item._id}
-        className="grid-cols-7"
-      >
+      <CoreTableRow handleSubmit={handleSubmit} onSubmit={onSubmit} key={item._id} className='grid-cols-7'>
         {entries.map(([key, value]) => {
           if (key === "image") {
             return (
-              <div className="core-center" key={key}>
-                <Image
-                  src={value}
-                  alt="Example local image"
-                  width={50}
-                  height={50}
-                  className="w-10 h-10 rounded-full object-cover"
-                  priority
-                />
+              <div className='core-center' key={key}>
+                <Image src={value} alt='Example local image' width={50} height={50} className='w-10 h-10 rounded-full object-cover' priority />
               </div>
             );
           }
           if (key === "sourceUrl") {
             return actionId ? (
-              <div className="flex items-center">
-                <button type="submit">Confirm</button>;
-                <button onClick={() => setActionId(null)}>Cancel</button>;
+              <div className='flex items-center'>
+                <button type='submit'>Confirm</button>;<button onClick={() => setActionId(null)}>Cancel</button>
               </div>
             ) : (
-              <div
-                key={key}
-                className="flex justify-end items-center px-10 gap-3"
-              >
+              <div key={key} className='flex justify-end items-center px-10 gap-3'>
                 <Link href={value}>
-                  <LinkIcon className="w-4 h-4" strokeWidth={1.5} />
+                  <LinkIcon className='w-4 h-4' strokeWidth={1.5} />
                 </Link>
-                <Pencil
-                  onClick={() => setActionId(item._id)}
-                  className="w-4 h-4"
-                  strokeWidth={1.5}
-                />
-                <Trash2
-                  onClick={() => deleteMutation.mutate(item?._id)}
-                  className="w-4 h-4"
-                  strokeWidth={1.5}
-                />
+                <Pencil onClick={() => setActionId(item._id)} className='w-4 h-4' strokeWidth={1.5} />
+                <Trash2 onClick={() => deleteMutation.mutate(item?._id)} className='w-4 h-4' strokeWidth={1.5} />
               </div>
             );
           }
           return actionId === item?._id ? (
-            <CoreInput
-              value={value}
-              key={key}
-              name={key}
-              register={register}
-              fieldType="flat"
-            />
+            <CoreInput value={value} key={key} name={key} register={register} fieldType='flat' />
           ) : (
             <CoreText key={key}>{value}</CoreText>
           );
@@ -119,18 +77,18 @@ const CoreTable = (props) => {
   });
 
   return (
-    <table className={root}>
-      <thead className={header}>
+    <div className={root}>
+      <div className={header}>
         {columns?.map((column, index) => (
-          <tr className="" key={index}>
-            <th onClick={() => sortingHandler()} className={headerItem}>
+          <div className='' key={index}>
+            <div onClick={() => sortingHandler()} className={headerItem}>
               {column}
-            </th>
-          </tr>
+            </div>
+          </div>
         ))}
-      </thead>
-      <tbody className="core-table-body">{loading ? loader : content}</tbody>
-    </table>
+      </div>
+      <div className='core-table-body'>{loading ? loader : content}</div>
+    </div>
   );
 };
 
--- src/components/CoreTable/CoreTableRow.js
+++ src/components/CoreTable/CoreTableRow.js
@@ -3,11 +3,9 @@ import React from "react";
 const CoreTableRow = ({ children, className, handleSubmit, onSubmit }) => {
   const styles = `core-table-row ${className}`;
   return (
-    <div>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles}>
-        {children}
-      </form>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)} className={styles}>
+      {children}
+    </form>
   );
 };
 

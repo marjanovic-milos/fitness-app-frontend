f9f108887fff1c9d4c65299b5c725c36a1141d4c
Milos Marjanovic <miskoo995@gmail.com>
2025-08-22 01:48:41 +0200

    login page edit

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -6,10 +6,13 @@ import { signIn } from "../../http/api/auth";
 import { useForm } from "react-hook-form";
 import styles from "./loginClient.module.css";
 import CoreInput from "src/components/CoreInput/CoreInput";
-
+import Image from "next/image";
 import CoreButton from "src/components/CoreButton/CoreButton";
 import { useRouter } from "next/navigation";
-
+import avatar from "../.././../public/sportive.png";
+import CoreText from "src/components/CoreText/CoreText";
+import CoreHeading from "src/components/CoreHeading/CoreHeading";
+import Link from "next/link";
 const ClientLogin = () => {
   const {
     register,
@@ -31,31 +34,43 @@ const ClientLogin = () => {
   const onSubmit = (data) => {
     mutate({ data });
   };
-  console.log(errors, "errors");
+  // console.log(errors, "errors");
   return (
     <div className={styles.root}>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <div className={"flex flex-col xl_w-[50%] w-full  text-white"}>
-          <CoreInput
-            name="email"
-            type="email"
-            label="Email"
-            register={register}
-
-            // {...register("email")}
-          />
-          <CoreInput
-            classes={{ root: "mt-2" }}
-            name="password"
-            register={register}
-            required={"Password is required field"}
-            // {...register("password")}
-            type="password"
-            label="Password"
-          />
-          <CoreButton classes={{ root: styles.button }}>Login</CoreButton>
+      <div className={styles.leftSide}>
+        {/* <CoreHeading variant='h1' className='h-auto py-10 font-bold'>
+          One stop shop, for all your managing!
+        </CoreHeading> */}
+        <Image src={avatar} alt='My Photo' width={400} height={300} />
+      </div>
+      <div className={styles.rightSide}>
+        <div className={styles.formWrap}>
+          <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+            <CoreHeading variant='h3' className='font-bold'>
+              Sign In
+            </CoreHeading>
+            <CoreInput name='email' type='email' label='Email' register={register} {...register("email")} />
+            <CoreInput
+              classes={{ root: "mt-2" }}
+              name='password'
+              register={register}
+              required={"Password is required field"}
+              {...register("password")}
+              type='password'
+              label='Password'
+            />
+            <CoreButton classes={"w-full"}>Login</CoreButton>
+            <div className='flex justify-center '>
+              <CoreText>
+                First time here?
+                <Link href='/client' className='flex justify-center underline mx-2'>
+                  Register first
+                </Link>
+              </CoreText>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
